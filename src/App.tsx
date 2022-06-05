0a2da400ff1628d8b5187a194129dbd5dafae68e
Marcos Botene <marcosjbotene@gmail.com>
2022-06-04 22:40:22 -0300

    created header component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Header } from './components';
+
 import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
---
+++ src/components/header/styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  background: var(--blue);
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  max-width: 1120px;
+
+  margin: 0 auto;
+
+  padding: 2rem 1rem 12rem;
+
+  button {
+    height: 3rem;
+    font-size: 1rem;
+
+    padding: 0 2rem;
+
+    border: 0;
+    border-radius: 0.25rem;
+
+    color: #fff;
+    background: var(--blue-light);
+
+    transition: filter 0.2;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`;
