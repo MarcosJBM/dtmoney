be580a787ff724550acebf06876d17006ec8b64e
Marcos Botene <marcosjbotene@gmail.com>
2022-06-04 23:27:21 -0300

    created transaction table component.

---
+++ src/components/transaction-table/index.tsx
@@ -0,0 +1,62 @@
+import { Container } from './styles';
+
+export function TransactionTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className='deposit'>R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className='withdraw'>- R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className='deposit'>R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className='withdraw'>- R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className='deposit'>R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className='withdraw'>- R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
---
+++ src/components/transaction-table/styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 4rem;
+
+  table {
+    width: 100%;
+    border-spacing: 0 0.5rem;
+
+    th {
+      font-weight: 400;
+
+      text-align: left;
+      line-height: 1.5rem;
+
+      padding: 1rem 2rem;
+
+      color: var(--text-body);
+    }
+
+    td {
+      padding: 1rem 2rem;
+
+      border: 0;
+      border-radius: 0.25rem;
+
+      color: var(--text-body);
+      background: var(--shape);
+
+      &:first-child {
+        color: var(--text-title);
+      }
+
+      &.deposit {
+        color: var(--green);
+      }
+
+      &.withdraw {
+        color: var(--red);
+      }
+    }
+  }
+`;
